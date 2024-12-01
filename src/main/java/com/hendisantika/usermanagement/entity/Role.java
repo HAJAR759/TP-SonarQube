69eee11d74528ab0e3db2bf40e93db67a8c63a70
hajar_elmechhouri <hajarelmech@gmail.com>
2024-12-01 20:23:41 +0100

    modifier role

--- src/main/java/com/hendisantika/usermanagement/entity/Role.java
+++ src/main/java/com/hendisantika/usermanagement/entity/Role.java
@@ -13,10 +13,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 import java.io.Serializable;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : user-management
- */
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -35,4 +32,33 @@ public class Role implements Serializable {
 
     @Column
     private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+    
 }
